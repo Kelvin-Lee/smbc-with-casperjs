fa33c518b5004e88e49a99af5cb352fc86c92992
Kelvin Lee <orangejuiceboy@gmail.com>
2016-07-16 19:14:24 -0700

    Echos the latest and 2nd latest comic title.

--- smbc.js
+++ smbc.js
@@ -3,6 +3,12 @@ casper.start('http://smbc-comics.com/');
 
 casper.then(function(){
   this.echo(this.getElementsAttribute('#cc-comic', 'title'));
+  this.click('#navbottom .nav a.prev[rel="prev"]');
+});
+
+casper.then(function(){
+  this.echo(this.getElementsAttribute('#cc-comic', 'title'));
+  this.click('#navbottom .nav a.prev[rel="prev"]');
 });
 
 casper.run();
