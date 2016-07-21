abd8810066aea814f3c14d0b403337c240f364f3
Kelvin Lee <orangejuiceboy@gmail.com>
2016-07-21 15:46:20 -0700

    Update README.md; organize script.

--- smbc.js
+++ smbc.js
@@ -1,12 +1,13 @@
 var casper = require('casper').create();
-casper.start('http://smbc-comics.com/');
-
-casper.then(getTitle);
 
 function getTitle(){
   this.echo(this.getElementsAttribute('#cc-comic', 'title'));
   this.click('#navbottom .nav a.prev[rel="prev"]');
   this.then(getTitle);
 };
+
+casper.start('http://smbc-comics.com/');
+
+casper.then(getTitle);
 
 casper.run();
