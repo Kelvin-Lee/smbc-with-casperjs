6fdd3477567d1a199a38842f20f3ba27fb2b1580
Kelvin Lee <orangejuiceboy@gmail.com>
2016-07-22 12:36:03 -0700

    Enable writing to file; Adds output (2016 July 22) to repo.

--- smbc.js
+++ smbc.js
@@ -1,13 +1,23 @@
+var fs = require('fs'); // Not node fs, check phantomjs api fs module
 var casper = require('casper').create();
 
+var file = fs.open('./smbc.txt', 'w');
+
 function getTitle(){
-  this.echo(this.getElementsAttribute('#cc-comic', 'title'));
-  this.click('#navbottom .nav a.prev[rel="prev"]');
-  this.then(getTitle);
+  var title = this.getElementsAttribute('#cc-comic', 'title');
+  file.writeLine(title);
+  this.echo(title);
+
+  if (this.exists('#navbottom .nav a.prev[rel="prev"]')){
+    this.click('#navbottom .nav a.prev[rel="prev"]');
+    this.then(getTitle);
+  } else {
+    file.close();
+    casper.exit();
+  }
 };
 
 casper.start('http://smbc-comics.com/');
-
 casper.then(getTitle);
-
 casper.run();
+
